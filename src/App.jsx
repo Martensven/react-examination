f71ea58d7bca43f262a09089c5b0326655bb8280
Martensven <martengullstrom@gmail.com>
2025-02-07 08:26:08 +0100

    removed unused route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import NavBar from "./components/navbar/navbar"
 import Home from "./components/home/home";
-import Projects from "./components/projects/projects";
 import About from "./components/about/about";
 import Contact from "./components/contact/contact";
 import Greeting from './components/Greeting/Greeting';
@@ -19,7 +18,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
-        <Route path="/Projects" element={<Projects />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Projects/Greeting" element={<Greeting />} />
         <Route path="/Projects/Memory" element={<Card />} />
